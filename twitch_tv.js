3b560e841139e3f2686daa5ef1f829dead7f3781
helio cardoso <cardoso_helio@hotmail.com>
2017-06-23 01:20:59 +0100

    Work on twitch api app

--- twitch_tv.js
+++ twitch_tv.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   var api_users = "https://api.twitch.tv/kraken/users/";
   var client_id = "7z9b84cn6ix9kei678z24y5rqhesi2";
   var api_version = "&v5+json";
-  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-  var user_call_stream, user_name, logo, url, game, bio, html_all, status, watching;
+  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "hearthstonefr", ];
+  var user_call_stream, user_name, logo, url, game, bio, html_all, status, watching, followers, views;
 
   // for (var i = 0; i < users.length; i++) {
   //   api_streams = api_streams.concat(users[i] + "?client_id=" + client_id + api_version);
@@ -46,7 +46,7 @@ $(document).ready(function() {
         $("#users_offline").hide();
         $("#users_online").hide();
 
-        html_all = "<div class='col-md-4 text-center' id='all_description'><ul class='list-group list-unstyled'><a href='" + url + "' target='_blank'><li class='list-group-item'><img class='img-responsive img-circle img_size' src='" + logo + "'></li><li class='list-group-item bg-inverse text-white'>" + user_name + "</li></a></ul></div><div class='col-md-8 text-center'><p class='list-group-item-text' style='position: relative; top: 40px'>" + bio + "</p></div>";
+        html_all = "<div class='col-md-4 text-center text-white' style='padding: 0'><ul class='list-group list-unstyled'><a href='" + url + "' target='_blank'><li class='list-group-item'><img class='img-responsive img-circle img_size' src='" + logo + "'></li><li class='list-group-item bg-inverse'>" + user_name + "</li></a></ul></div><div class='col-md-8'><p class='list-group-item-text text-center text-justify text-black' style='position: relative; top: 40px'>" + bio + "</p></div>";
 
         return $("#users_all").show().html(html_all);
       });
@@ -65,7 +65,7 @@ $(document).ready(function() {
           $("#users_all").hide();
           $("#users_online").hide();
 
-          html = "<div class='col-md-4 text-center'><ul class='list-group list-unstyled'><a href='" + url + "' target='_blank'><li class='list-group-item'><img class='img-responsive img-circle img_size' src='" + logo + "'></li><li class='list-group-item bg-warning text-white'>" + user_name + "</li></a></ul></div><div class='col-md-8 text-center'><p class='list-group-item-text' style='position: relative; top: 70px'>Currently Offline</p></div>";
+          html = "<div class='col-md-4 text-white text-center' style='padding: 0'><ul class='list-group list-unstyled'><a href='" + url + "' target='_blank'><li class='list-group-item'><img class='img-responsive img-circle img_size border-logo-offline' src='" + logo + "'></li><li class='list-group-item bg-warning'>" + user_name + "</li></a></ul></div><div class='col-md-8'><p class='list-group-item-text text-black text-center' style='position: relative; top: 70px'>Currently Offline</p></div>";
 
           return $("#users_offline").show().html(html);
         });
@@ -84,11 +84,15 @@ $(document).ready(function() {
           game = data_online.stream.game;
           status = data_online.stream.channel.status;
           watching = data_online.stream.viewers;
+          followers = data_online.stream.channel.followers;
+          views = data_online.stream.channel.views;
 
           $("#users_all").hide();
           $("#users_offline").hide();
 
-          html = "<div class='col-md-4 text-center'><ul class='list-group list-unstyled'><a href='" + url + "' target='_blank'><li class='list-group-item'><img class='img-responsive img-circle img_size' src='" + logo + "'></li><li class='list-group-item bg-success text-white'>" + user_name + "</li></a></ul></div><div class='col-md-8 text-center'><ul class='list-group list-unstyled'><li class='list-group-item-text'>" + status + "</li><li class='list-group-item-text' style='position: relative; top: 30px'>Currently playing <em><strong>" + game + "</strong></em></li><li class='list-group-item-text' style='position: relative; top: 40px'><div><p>Currently watching: " + watching + " <i class='fa fa-user' aria-hidden='true'></i></div></li></ul></div>";
+          html = "<div class='col-md-4 text-white text-center' style='padding: 0'><ul class='list-group list-unstyled'><a href='" + url + "' target='_blank'><li class='list-group-item'><img class='img-responsive img-circle img_size border-logo-online' src='" + logo + "'></li><li class='list-group-item bg-success'>" + user_name + "</li></a></ul></div><div class='col-md-8 text-center'><ul class='list-group list-unstyled' style='height: 50px'><li class='list-group-item-text'><p class='text-black'>" + status + "</p></li><li class='list-group-item-text' style='position: relative; top: 30px'><p class='text-black'>Currently playing: <strong><span class='text-info'>" + game + "</span></strong></p></li></ul></div>";
+
+          html += "<div class='row'><div class='col-md-4 text-center list-group-item-text'><p class='text-black'>Currently watching: <em class='text-info'>" + watching + "</em> <i class='fa fa-user fa-lg text-info' aria-hidden='true'></i></div><div class='col-md-4 text-center'><p class='text-black'>Followers: <em class='text-info'>" + followers + "</em> <i class='fa fa-users fa-lg text-info' aria-hidden='true'></i></p></div><div class='col-md-4 text-center'><p class='text-black'>Views: <em class='text-info'>" + views + "</em> <i class='fa fa-eye fa-lg text-info' aria-hidden='true'></i></p></div></div>";
 
           return $("#users_online").show().html(html);
         });
